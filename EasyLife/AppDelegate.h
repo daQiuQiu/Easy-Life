fb81e8f835f1d76fbaddb2bc151f69a2ade33b29
your name <your_email@youremail.com>
2016-01-11 14:38:38 +0800

    Initial Commit

---
+++ EasyLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyLife
+//
+//  Created by 易仁 on 16/1/11.
+//  Copyright © 2016年 易仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyLife
+//
+//  Created by 易仁 on 16/1/11.
+//  Copyright © 2016年 易仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
